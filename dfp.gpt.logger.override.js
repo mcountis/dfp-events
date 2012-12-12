7f3a49e93cb5a0b68e9ff150d2c8f40df589ddbe
Mike Countis <mike.countis@gmail.com>
2012-12-12 10:51:03 -0800

    Update dfp.gpt.logger.override.js

--- dfp.gpt.logger.override.js
+++ dfp.gpt.logger.override.js
@@ -11,26 +11,26 @@
 	
 		var events = [];
 		
-		addEvent("gpt-google_js_loaded",			              /Google service JS loaded/ig);
-		addEvent("gpt-gpt_fetch",                   				/Fetching GPT implementation/ig);
-		addEvent("gpt-gpt_fetched",				                  /GPT implementation fetched./ig);
-		addEvent("gpt-page_load_complete",            			/Page load complete/ig);
-		addEvent("gpt-queue_start",				                  /Invoked queued function. Total: 3 Errors: 0/ig);
+		addEvent("gpt-google_js_loaded",/Google service JS loaded/ig);
+		addEvent("gpt-gpt_fetch",/Fetching GPT implementation/ig);
+		addEvent("gpt-gpt_fetched",/GPT implementation fetched./ig);
+		addEvent("gpt-page_load_complete",/Page load complete/ig);
+		addEvent("gpt-queue_start",/Invoked queued function. Total: 3 Errors: 0/ig);
 	
-		addEvent("gpt-service_add_slot",              			/Associated (.*) service with slot (.*)/ig);
-		addEvent("gpt-service_add_targeting",			          /Setting targeting attribute (.*) with value (.*) for service (.*)/ig);
-		addEvent("gpt-service_collapse_containers_enable",	/Enabling collapsing of containers when there is no ad content/ig);
-		addEvent("gpt-service_create",              				/Created service: (.*)/ig);
-		addEvent("gpt-service_single_request_mode_enable",	/Using single request mode to fetch ads/ig);
+		addEvent("gpt-service_add_slot",/Associated (.*) service with slot (.*)/ig);
+		addEvent("gpt-service_add_targeting",/Setting targeting attribute (.*) with value (.*) for service (.*)/ig);
+		addEvent("gpt-service_collapse_containers_enable",/Enabling collapsing of containers when there is no ad content/ig);
+		addEvent("gpt-service_create",/Created service: (.*)/ig);
+		addEvent("gpt-service_single_request_mode_enable",/Using single request mode to fetch ads/ig);
 	
-		addEvent("gpt-slot_create",				                  /Created slot: (.*)/ig);
-		addEvent("gpt-slot_add_targeting",			            /Setting targeting attribute (.*) with value (.*) for slot (.*)/ig);
-		addEvent("gpt-slot_fill",				                    /Calling fillslot/ig);
-		addEvent("gpt-slot_fetch",                          /Fetching ad for slot (.*)/ig);
-		addEvent("gpt-slot_receiving",				              /Receiving ad for slot (.*)/ig);
-		addEvent("gpt-slot_render_delay",			              /Delaying rendering of ad slot (.*) pending loading of the GPT implementation/ig);
-		addEvent("gpt-slot_rendering",				              /Rendering ad for slot (.*)/ig);
-		addEvent("gpt-slot_rendered",				                /Completed rendering ad for slot (.*)/ig);
+		addEvent("gpt-slot_create",/Created slot: (.*)/ig);
+		addEvent("gpt-slot_add_targeting",/Setting targeting attribute (.*) with value (.*) for slot (.*)/ig);
+		addEvent("gpt-slot_fill",/Calling fillslot/ig);
+		addEvent("gpt-slot_fetch",/Fetching ad for slot (.*)/ig);
+		addEvent("gpt-slot_receiving",/Receiving ad for slot (.*)/ig);
+		addEvent("gpt-slot_render_delay",/Delaying rendering of ad slot (.*) pending loading of the GPT implementation/ig);
+		addEvent("gpt-slot_rendering",/Rendering ad for slot (.*)/ig);
+		addEvent("gpt-slot_rendered",/Completed rendering ad for slot (.*)/ig);
 	
 		googletag.debug_log.log = function(level,message,service,slot,reference){
 			var _arguments = Array.prototype.splice.call(arguments);
