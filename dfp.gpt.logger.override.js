3d5bf2b7bafb8ee4eb6754d9aab166c8b6026445
Mike Countis <mike.countis@gmail.com>
2013-03-19 10:33:48 -0700

    Remove jQuery dependence
    removed use of jQuery.trigger and instead monkey-patch the googletag object with methods 'on', 'off', and 'trigger' and value 'events'.
    after running through jslint also cleaned up regexes and var declarations.

--- dfp.gpt.logger.override.js
+++ dfp.gpt.logger.override.js
@@ -1,48 +1,115 @@
-(function($){
+(function(){
+	"use strict";
+
 	window.googletag = window.googletag || {};
 	window.googletag.cmd = window.googletag.cmd || [];
 	
 	googletag.cmd.push(function(){
-	
-		var _log = googletag.debug_log.log;
-		var addEvent = function(name,match){
-			events.push({
-				"name":name,
-				"match":match
-			});
-		}
-	
-		var events = [];
 		
+		if(googletag.hasOwnProperty("on") || googletag.hasOwnProperty("off") || googletag.hasOwnProperty("trigger") || googletag.hasOwnProperty("events"))
+			return;
+		
+		var 	old_log = googletag.debug_log.log,
+			events = [],
+			addEvent = function(name,match){
+				events.push({
+					"name":name,
+					"match":match
+				});
+			};
+
 		addEvent("gpt-google_js_loaded",			/Google service JS loaded/ig);
 		addEvent("gpt-gpt_fetch",				/Fetching GPT implementation/ig);
-		addEvent("gpt-gpt_fetched",				/GPT implementation fetched./ig);
+		addEvent("gpt-gpt_fetched",				/GPT implementation fetched\./ig);
 		addEvent("gpt-page_load_complete",			/Page load complete/ig);
 		addEvent("gpt-queue_start",				/^Invoked queued function/ig);
-	
-		addEvent("gpt-service_add_slot",			/Associated (.*) service with slot (.*)/ig);
-		addEvent("gpt-service_add_targeting",			/Setting targeting attribute (.*) with value (.*) for service (.*)/ig);
+
+		addEvent("gpt-service_add_slot",			/Associated ([\w]*) service with slot ([\/\w]*)/ig);
+		addEvent("gpt-service_add_targeting",			/Setting targeting attribute ([\w]*) with value ([\w\W]*) for service ([\w]*)/ig);
 		addEvent("gpt-service_collapse_containers_enable",	/Enabling collapsing of containers when there is no ad content/ig);
-		addEvent("gpt-service_create",				/Created service: (.*)/ig);
+		addEvent("gpt-service_create",				/Created service: ([\w]*)/ig);
 		addEvent("gpt-service_single_request_mode_enable",	/Using single request mode to fetch ads/ig);
-	
-		addEvent("gpt-slot_create",				/Created slot: (.*)/ig);
-		addEvent("gpt-slot_add_targeting",			/Setting targeting attribute (.*) with value (.*) for slot (.*)/ig);
+
+		addEvent("gpt-slot_create",				/Created slot: ([\/\w]*)/ig);
+		addEvent("gpt-slot_add_targeting",			/Setting targeting attribute ([\w]*) with value ([\w\W]*) for slot ([\/\w]*)/ig);
 		addEvent("gpt-slot_fill",				/Calling fillslot/ig);
-		addEvent("gpt-slot_fetch",				/Fetching ad for slot (.*)/ig);
-		addEvent("gpt-slot_receiving",				/Receiving ad for slot (.*)/ig);
-		addEvent("gpt-slot_render_delay",			/Delaying rendering of ad slot (.*) pending loading of the GPT implementation/ig);
-		addEvent("gpt-slot_rendering",				/^Rendering ad for slot (.*)/ig);
-		addEvent("gpt-slot_rendered",				/Completed rendering ad for slot (.*)/ig);
-		
-		googletag.debug_log.log = function(level,message,service,slot,reference){
-			var _arguments = Array.prototype.slice.call(arguments);
-			for(var e = 0,el = events.length;e < el; e++)
-				if(message.search(events[e].match) > -1){
-					$(googletag).trigger(events[e].name,_arguments);
-				}
-			return _log.apply(this,arguments);
+		addEvent("gpt-slot_fetch",				/Fetching ad for slot ([\/\w]*)/ig);
+		addEvent("gpt-slot_receiving",				/Receiving ad for slot ([\/\w]*)/ig);
+		addEvent("gpt-slot_render_delay",			/Delaying rendering of ad slot ([\/\w]*) pending loading of the GPT implementation/ig);
+		addEvent("gpt-slot_rendering",				/^Rendering ad for slot ([\/\w]*)/ig);
+		addEvent("gpt-slot_rendered",				/Completed rendering ad for slot ([\/\w]*)/ig);
+
+		googletag.events = googletag.events || {};
+
+		googletag.on = function(events,op_arg0/*data*/,op_arg1/*callback*/){
+			if(!op_arg0)
+				return this;
+
+			events = events.split(" ");
+
+			var	data = op_arg1 ? op_arg0 : undefined,
+				callback = op_arg1 || op_arg0,
+				ei = 0,e = '';
+			
+			callback.data = data;
+
+			for(e = events[ei = 0]; ei < events.length; e = events[++ei])
+				(this.events[e] = this.events[e] || []).push(callback);
+
+			return this;
 		};
-	
+
+
+		googletag.off = function(events,handler){
+			events = events.split(" ");
+			var	ei = 0,e = "",
+				fi = 0,f = function(){};
+			
+			for(e = events[ei]; ei < events.length; e = events[++ei]){
+				if(!this.events.hasOwnProperty(e))
+					continue;
+
+				if(!handler){
+					delete this.events[e];
+					continue;
+				}
+
+				fi = this.events[e].length - 1;
+				for(f = this.events[e][fi]; fi >= 0; f = this.events[e][--fi])
+					if(f == handler)
+						this.events[e].splice(fi,1);
+				if(this.events[e].length === 0)
+					delete this.events[e];
+			}
+
+			return this;
+		};
+
+
+		googletag.trigger = function(event,parameters){
+
+			if(!this.events[event] || this.events[event].length === 0)
+				return this;
+			
+			var	parameters = parameters || [],
+				fi = 0,f = this.events[event][fi];
+			
+			for(fi,f;fi < this.events[event].length;f = this.events[event][++fi])
+				if(f.apply(this,[{data:f.data}].concat(parameters)) === false)
+					break;
+
+			return this;
+		};
+
+
+		googletag.debug_log.log = function(level,message,service,slot,reference){
+			var	args = Array.prototype.slice.call(arguments),
+				e = 0;
+			for(e;e < events.length; e++)
+				if(message.search(events[e].match) > -1)
+					googletag.trigger(events[e].name,args);
+			return old_log.apply(this,arguments);
+		};
+
 	});
-})(jQuery);
+})();
